4259c9bee1af520395bc6664f384841e67637e04
Singh0Himanshu <shimanshu.3689@gmail.com>
2025-09-05 22:58:36 +0530

    useEffect chnages

--- src/components/UseEffects.jsx
+++ src/components/UseEffects.jsx
@@ -8,7 +8,7 @@ const UseEffects = () => {
         setTimeout(()=>{
             setCount(count+1);
         },5000);
-    },[])
+    },[]) // if I remove the dependency than it will work  like timer and keep increasing the timer by 1 every time.
   return (
     <div>
       <h1>I hav rendered {count} time</h1>
