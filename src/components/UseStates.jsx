4c8d377b51a2aeb5e409c274bf1c39c1527a73e1
Singh0Himanshu <shimanshu.3689@gmail.com>
2025-09-05 10:22:25 +0530

    Add Initial Files

---
+++ src/components/UseStates.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+
+const UseStates = () => {
+    const [color,setColor] = useState('red');
+    const [buttonColor,setButtonColor] = useState("blue");
+
+    const [car,setCar] = useState({
+        brand:"ferrari",
+        year:2023,
+        color:"red",
+        model:"roma"
+    })
+
+    const changeColor = ()=>{
+        color === "red"? setColor("blue"):setColor("red");
+        buttonColor === "blue"? setButtonColor("red"):setButtonColor("blue");
+    }
+
+    const handleData = ()=>{
+        setCar({...car,color:"blue"});
+        console(car);
+    }
+
+  return (
+    <>
+      <h1>Hello , I am Himanshu Singh</h1>
+      <p>I love {color} color.</p>
+      <button type="button" onClick={changeColor}>{buttonColor}</button>
+      <br/>
+      <h1>My {car.brand}</h1>
+      <p>It is {car.color} {car.model} from {car.year}</p>
+      <button type="button" onClick={handleData}>change data</button>
+
+    </>
+  )
+}
+
+export default UseStates
